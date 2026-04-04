dd1792762f77d2de15faad42ac74c92151f90d91
Pramod <whoisinthevips@gmail.com>
2026-04-04 19:35:37 +0530

    completed array basics

--- basics/array_basics.c
+++ basics/array_basics.c
@@ -2,13 +2,17 @@
 int main(){
     int A[]={1,2,3,4,5,6,7,8,9,0};//This can also be initialised as this 
     int B[5]={1,2,3,4};//2nd method
-    b[5]=5;//also intialised after declaration
+    B[4]=5;//also intialised after declaration
     
-    int C[10]={0}//All element are intialised as 0
+    int C[10]={0};//All element are intialised as 0
 
-    printf("%d",C);//print all the elemnt in array C 
+    printf("%d\n",C[2]);//print 3rd elemnt in array C which is at index 2nd
+
+    //Now printing all the element in array b
+    int i;
+    for(int i=0;i<5;i++){
+        printf("%d\n",B[i]);
+    }
     
-
-
     return 0;
 }

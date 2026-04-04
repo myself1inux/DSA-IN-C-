bd4cfe5445b635d14384d7ac5c12cbec65a8dfd1
Pramod <whoisinthevips@gmail.com>
2026-04-04 18:57:13 +0530

    Added code for initialision, delaration and empty array

---
+++ basics/array_basics.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int A[]={1,2,3,4,5,6,7,8,9,0};//This can also be initialised as this 
+    int B[5]={1,2,3,4};//2nd method
+    b[5]=5;//also intialised after declaration
+    
+    int C[10]={0}//All element are intialised as 0
+
+    printf("%d",C);//print all the elemnt in array C 
+    
+
+
+    return 0;
+}

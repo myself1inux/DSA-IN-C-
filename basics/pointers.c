95c30499b1e4651dcbdb22988e37bd916e862417
Pramod <whoisinthevips@gmail.com>
2026-04-06 00:31:40 +0530

    Creating array and accesing heap section of th e memory using pointer

--- basics/pointers.c
+++ basics/pointers.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
     int a=10;
     int *p;//Declaration of a pointer 
@@ -43,6 +44,30 @@ int main(){
     //Method 2
     for(i=0;i<5;i++){
         printf("%d\n",b[i]);//Works same as Method 1
+    }
+
+    //Creating array inside the heap section of the memory
+    //Since we are using malloc function in the program so we have to include stdlib named library for that
+    //Comment till line 46 from line 3 for better understanding 
+    int *c;//Creating pointer 
+    c=(int *)malloc(5*sizeof(int));
+    //Here we are creating a array in the heap section of the memory using malloc function 
+    //The size of the array is 5 and the type or array is interger
+    //Since malloc function return void type pointer so that we have to type cast it into int pointer.
+    //And finally assigning it to c pointer
+
+
+    //Initialising the values
+    c[0]=10;
+    c[1]=20;
+    c[2]=30;
+    c[3]=40;
+    c[4]=50;
+
+    //Printing the values
+
+    for(i=0;i<5;i++){
+        printf("%d\n",c[i]);
     }
     return 0;
 }

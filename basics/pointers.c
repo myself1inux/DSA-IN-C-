f64eb3c7604ce26d85c55150b9b91f2812dcb70c
Pramod <whoisinthevips@gmail.com>
2026-04-05 23:52:56 +0530

    Intialising pointer to an array by 2 methods

--- basics/pointers.c
+++ basics/pointers.c
@@ -17,5 +17,20 @@ int main(){
     printf("%d is the adderss of variable a\n",&a);
     //Method 2 
     printf("%d is the adderss of variable a\n",p);
+
+
+    //Pointer to an array
+    //Comment till line 19 from line 3 for better understanding 
+    
+    int A[5]={1,2,3,4,5};
+    int *b;
+    //Initialising pointer to an array
+    //Method 1
+
+    b=A; // Here we don't need to put &(ampersand) because the array A itself contains the address of its first element
+
+    //Method 2
+    b=&A[0];//Here we need to add &(ampersand) because we are specifying the index of the array.
+
     return 0;
 }

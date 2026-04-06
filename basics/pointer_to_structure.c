e39f3750866a4be267424aeb2fd27ce4ef45e5e7
Pramod <whoisinthevips@gmail.com>
2026-04-06 23:42:50 +0530

    Creating a structure and its type of pointer

---
+++ basics/pointer_to_structure.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    struct rectangle {
+        int length;
+        int breadth;
+    };
+    struct rectangle r1;
+    struct rectangle *p; //Here we are creating a pointer which stores the address of a structure named rectangle
+    
+
+
+    return 0;
+}

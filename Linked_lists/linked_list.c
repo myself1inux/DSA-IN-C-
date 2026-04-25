59614817ba6cd25726d0c74c987c81bbb4a82e15
Pramod <whoisinthevips@gmail.com>
2026-04-25 17:37:50 +0530

    Created a linked list with three node

--- Linked_lists/linked_list.c
+++ Linked_lists/linked_list.c
@@ -1,1 +1,22 @@
 //CREATING A LINKED LIST 
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *head,*second,*third;
+    head = (struct node*)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    head->data = 10;
+    head->next = second;
+    second->data = 20;
+    second->next = third;
+    third->data = 30;
+    third->next = NULL;
+    return 0;
+}
